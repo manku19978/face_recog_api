16944f782bd8407f40c0d97bbd5165964e0617a9
Mayank Singh <mayanksingh1997@gmail.com>
2019-10-02 03:48:08 +0530

    adding server

---
+++ script.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const pg = require('knex')({
+    client: 'pg',
+    connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '1847ss',
+    database : 'test'
+  }
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('it is working');
+})
+
+app.post('/signin', (req, res) => {
+	pg.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data =>{
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if(isValid){
+				return pg.select('*').from('users')
+				  .where('email', '=', req.body.email)
+				  .then(user => {
+				  	res.json(user[0])
+				  })
+				  .catch(err => res.status(400).json('unable to get user'))
+			}else{
+				res.status(400).json('wrong credentials')
+			}
+		})
+		.catch(err=> res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	const hash = bcrypt.hashSync(password);
+	pg.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+					.returning('*')
+					.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date()
+					})
+					.then(user => {
+						res.json(user[0]);		
+					})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json('unable to register'))
+})
+
+app.get('/profile/:id', (req, res) => {
+	const {id} = req.params;
+	pg.select('*').from('users').where({id})
+		.then(user => {
+			if(user.length)
+				res.json(user[0])
+			else
+				res.status(400).json('Not found')
+		})
+		.catch(err => res.status(400).json('error getting user'))
+})
+
+app.put('/image', (req, res) => {
+	const {id} = req.body;
+	pg('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json('unable to get entries'))
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+	console.log("My first server");
+})
+
+
+
+
+
+/*
+
+/  --> res = this is working
+/signin --> post = success/fail
+/register --> post = user
+/profile/:userID --> get = user
+/image --> put -->user
+
+*/
